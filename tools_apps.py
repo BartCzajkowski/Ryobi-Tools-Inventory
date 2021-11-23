85481ebbe4b996bd1980096b29cdf0815d59b035
Bartosz Czajkowski <88278871+BartCzajkowski@users.noreply.github.com>
2021-11-23 11:12:56 +0100

    Yet another update.
    Added prices and some functionality with it.

--- tools_apps.py
+++ tools_apps.py
@@ -1,4 +1,4 @@
-"""A full list of apps"""
+"""A full list of apps."""
 import json
 
 with open('database.json', 'r', encoding="UTF-8") as file:
@@ -7,6 +7,35 @@ with open('database.json', 'r', encoding="UTF-8") as file:
     batteries = json.load(file)["batteries"]
 with open('database.json', 'r', encoding="UTF-8") as file:
     chargers = json.load(file)["chargers"]
+
+class Tool():
+    """Class used to describe a tool with all important parameters"""
+    catalog_id: str
+    use: str
+    brushless: bool
+    owned: bool
+    high_power: bool
+    wanted: bool
+    price: int
+
+class Battery():
+    """Class used to describe a battery with all important parameters"""
+    catalog_id: str
+    capacity: float
+    owned: bool
+    high_power: bool
+    high_energy: bool
+    wanted: bool
+    price: int
+
+class Charger():
+    """Class used to describe a charger with all important parameters"""
+    catalog_id: str
+    power: float
+    owned: bool
+    ports_count: bool
+    wanted: bool
+    price: int
 
 def missing_items():
     """Function returning any items that are wanted but not owned"""
@@ -56,6 +85,7 @@ def toolbox():
     for tool in tools:
         if tool["owned"]:
             owned_tools.append(tool["catalog_id"])
+    # nieudana próba skrócenia owned_tools.append((t["catalog_id"]) for t in tools if t["owned"])
     #batteries
     owned_batteries = []
     for battery in batteries:
@@ -68,3 +98,49 @@ def toolbox():
             owned_chargers.append(charger["catalog_id"])
     print(f"Oto lista posiadanych narzędzi:\nNarzędzia: {owned_tools}\n\
 Baterie: {owned_batteries}\nŁadowarki: {owned_chargers}")
+
+def tool_appending(tool: Tool):
+    """NOT WORKING Function designed to append a new tool into the database"""
+    new_tool = {
+        "catalog_id": tool.catalog_id,
+        "use": tool.use,
+        "brushless": tool.brushless,
+        "owned": tool.owned,
+        "high_power": tool.high_power,
+        "wanted": tool.wanted,
+        "price": tool.price
+    }
+    tools.append(new_tool)
+    with open('database.json', 'w', encoding="UTF-8") as new_file:
+        json.dump(tools, new_file)
+
+def value():
+    """Function used to print a full value of owned tools"""
+    sum_value = 0
+    for tool in tools:
+        if tool["owned"]:
+            sum_value += tool["price"]
+    for battery in batteries:
+        if battery["owned"]:
+            sum_value += battery["price"]
+    for charger in chargers:
+        if charger["owned"]:
+            sum_value += charger["price"]
+    print(f"Sumaryczna wartość posiadanych produktów Ryobi to {sum_value} złotych")
+
+def money_buy():
+    """function returning money needed to buy all tools which are not owned but wanded."""
+    sum_value = 0
+    for tool in tools:
+        if not tool["owned"]:
+            if tool["wanted"]:
+                sum_value += tool["price"]
+    for battery in batteries:
+        if not battery["owned"]:
+            if battery["wanted"]:
+                sum_value += battery["price"]
+    for charger in chargers:
+        if not charger["owned"]:
+            if charger["wanted"]:
+                sum_value += charger["price"]
+    print(f"Sumaryczna wartość produktów Ryobi, które trzeba dokupić to {sum_value} złotych")
